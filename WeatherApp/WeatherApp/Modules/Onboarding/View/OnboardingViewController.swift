e9fa879a3b67256aeedbcfc0c4413d905b9655d3
Sergey Balashov <sergey.balashov@instamart.ru>
2022-01-24 19:44:06 +0300

    add UI Vadim

--- WeatherApp/WeatherApp/Modules/Onboarding/View/OnboardingViewController.swift
+++ WeatherApp/WeatherApp/Modules/Onboarding/View/OnboardingViewController.swift
@@ -6,34 +6,131 @@
 //
 
 import UIKit
+import Foundation
 
 class OnboardingViewController: BaseViewController {
     private let presenter: OnboardingViewOutput
     
-    private lazy var requestLocation: UIButton = {
+    let onboardingScrollView: UIScrollView = {
+        var scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    let onboardingConteinerView: UIView  = {
+        var conteinerView = UIView()
+        conteinerView.translatesAutoresizingMaskIntoConstraints = false
+        return conteinerView
+    }()
+    private lazy var requestLocationButton: UIButton = {
         let view = UIButton(type: .system)
-        view.backgroundColor = UIColor.orange.withAlphaComponent(0.9)
+        view.backgroundColor = UIColor(named: "orangeColor")
         view.layer.cornerRadius = 8
-        view.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
-        
-        view.setTitle("Использовать геопозицию", for: .normal)
-        view.frame.size = CGSize(width: 200, height: 100)
+        view.setTitleColor(.white, for: .normal)
+        view.addTarget(self, action: #selector(tapButtonRequest), for: .touchUpInside)
+        view.setTitle("ИСПОЛЬЗОВАТЬ МЕСТОПОЛОЖЕНИЕ УСТРОЙСТВА", for: .normal)
+        view.titleLabel!.font = UIFont(name: "Rubik-Regular", size: 12)
+        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
+    private lazy var customLocationButton: UIButton = {
+        let view = UIButton(type: .system)
+        view.setTitleColor(.white, for: .normal)
+        view.addTarget(self, action: #selector(tapButtonCustom), for: .touchUpInside)
+        view.setTitle("НЕТ, Я БУДУ ДОБАВЛЯТЬ ЛОКАЦИИ", for: .normal)
+        view.titleLabel!.font = UIFont(name: "Rubik-Regular", size: 16)
+        view.titleLabel?.textAlignment = .right
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private var onboardingImage: UIImageView = {
+        let image = UIImageView(image: UIImage(named: "Frame")!)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private var onboardingStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis  = NSLayoutConstraint.Axis.vertical
+        view.spacing = 30
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private var onboardingTextLabal1: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor(red: 0.973, green: 0.961, blue: 0.961, alpha: 1)
+        label.font = UIFont(name: "Rubik-Regular", size: 16)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.text = "Разрешить приложению  Weather использовать данные о местоположении вашего устройства"
+        return label
+    }()
+    private var onboardingTextLabal2: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Чтобы получить более точные прогнозы погоды во время движения или путешествия \n\nВы можете изменить свой выбор в любое время из меню приложения"
+        label.textColor = .white
+        label.font = UIFont(name: "Rubik", size: 14)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
     init(presenter: OnboardingViewOutput) {
         self.presenter = presenter
-
+        
         super.init()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        view.addSubview(requestLocation)
-
-        requestLocation.center = view.center
+        view.backgroundColor = UIColor(named: "blueColor")
+        view.addSubview(onboardingScrollView)
+        onboardingScrollView.addSubview(onboardingConteinerView)
+        onboardingConteinerView.addSubview(onboardingImage)
+        onboardingConteinerView.addSubview(onboardingStackView)
+        onboardingStackView.addArrangedSubview(onboardingTextLabal1)
+        onboardingStackView.addArrangedSubview(onboardingTextLabal2)
+        onboardingConteinerView.addSubview(requestLocationButton)
+        onboardingConteinerView.addSubview(customLocationButton)
+        
+        setUpConstraints()
     }
+    func setUpConstraints() {
+        let constraints = [
+            onboardingImage.centerXAnchor.constraint(equalTo: onboardingConteinerView.centerXAnchor),
+            onboardingImage.topAnchor.constraint(equalTo: onboardingConteinerView.topAnchor, constant: 62),
+            
+            onboardingScrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            onboardingScrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            onboardingScrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            onboardingScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            onboardingConteinerView.topAnchor.constraint(equalTo: onboardingScrollView.topAnchor),
+            onboardingConteinerView.leadingAnchor.constraint(equalTo:  onboardingScrollView.leadingAnchor),
+            onboardingConteinerView.trailingAnchor.constraint(equalTo:  onboardingScrollView.trailingAnchor),
+            onboardingConteinerView.bottomAnchor.constraint(equalTo:  onboardingScrollView.bottomAnchor),
+            onboardingConteinerView.widthAnchor.constraint(equalTo:  onboardingScrollView.widthAnchor),
+            
+            onboardingStackView.topAnchor.constraint(equalTo: onboardingImage.bottomAnchor, constant: 30),
+            onboardingStackView.leadingAnchor.constraint(equalTo: onboardingConteinerView.leadingAnchor, constant: 19),
+            onboardingStackView.trailingAnchor.constraint(equalTo: onboardingConteinerView.trailingAnchor, constant: -16),
+            
+            requestLocationButton.topAnchor.constraint(equalTo: onboardingStackView.bottomAnchor, constant: 40),
+            requestLocationButton.leadingAnchor.constraint(equalTo: onboardingConteinerView.leadingAnchor, constant: 18),
+            requestLocationButton.trailingAnchor.constraint(equalTo: onboardingConteinerView.trailingAnchor, constant: -17),
+            requestLocationButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            customLocationButton.topAnchor.constraint(equalTo: requestLocationButton.bottomAnchor, constant: 25),
+            customLocationButton.trailingAnchor.constraint(equalTo: onboardingConteinerView.trailingAnchor, constant: -17),
+            customLocationButton.bottomAnchor.constraint(equalTo: onboardingConteinerView.safeAreaLayoutGuide.bottomAnchor, constant: -12)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -45,8 +142,10 @@ class OnboardingViewController: BaseViewController {
         navigationController?.setNavigationBarHidden(false, animated: false)
     }
     
-    @objc private func tapButton() {
+    @objc private func tapButtonRequest() {
         presenter.reqeustLocationButton()
+    }
+    @objc private func tapButtonCustom() {
     }
 }
 
