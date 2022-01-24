88ba8c643acff2103d1db0d254286e6d8c77af70
Sergey Balashov <sergey.balashov@instamart.ru>
2022-01-24 22:22:58 +0300

    fix output

---
+++ WeatherApp/Modules/Root/Interface/RootNavigationViewInput.swift
@@ -0,0 +1,13 @@
+//
+//  RootNavigationViewInput.swift
+//  WeatherApp
+//
+//  Created by Sergey Balashov on 24.01.2022.
+//
+
+import Foundation
+
+protocol RootNavigationViewInput: AnyObject {
+    func startMain()
+    func startOnboarding()
+}
---
+++ WeatherApp/Modules/Root/Interface/RootNavigationViewOutput.swift
@@ -0,0 +1,12 @@
+//
+//  RootNavigationViewOutput.swift
+//  WeatherApp
+//
+//  Created by Sergey Balashov on 24.01.2022.
+//
+
+import Foundation
+
+protocol RootNavigationViewOutput {
+    func viewDidLoad()
+}
